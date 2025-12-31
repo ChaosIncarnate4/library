99b30591e919017bc564de823a9417d3e3be9c1e
Wilson <wj0503@gmail.com>
2025-12-30 23:13:08 -0500

    Added form to submit new books

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const library = [];
+const libraryTable = document.querySelector('#libaryTable');
 
 function Book(title, author, pubDate, genre) {
     this.id = crypto.randomUUID();
@@ -13,11 +14,24 @@ function addBookToLibrary(title, author, pubDate, genre) {
     library.push(book);
 }
 
+function addBookFromForm() {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const year = document.getElementById('year').value;
+    const genre = document.getElementById('genre').value;
+    addBookToLibrary(title, author, year, genre);
+    displayLibrary();
+}
+
 addBookToLibrary("Percy Jackson", "Rick Riordan", 2005, "Urban Fantasy");
+addBookToLibrary("Hunger Games", "Suzanne Collins", 2007, "Dystopian");
 
-function displayLibrary() {
-    const libraryTable = document.querySelector('#libaryTable');
-
+function displayLibrary() {    
+    // Clear existing rows except the header
+    while (libraryTable.rows.length > 1) {
+        libraryTable.deleteRow(1);
+    }
+    
     library.forEach((book) => {
         console.log(book.title);
         let row = libraryTable.insertRow();
